260259f3e949247852a0591f3181b96b8f53158b
Shahrukh Raza <shahrukh4062@gmail.com>
2023-04-29 17:01:26 +0900

    done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sraza <sraza@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 10:05:28 by razasharuku       #+#    #+#             */
-/*   Updated: 2023/04/29 14:19:33 by sraza            ###   ########.fr       */
+/*   Updated: 2023/04/29 16:58:24 by sraza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@ int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	int		i;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_win = mlx_new_window(mlx, 1000, 1000, "NAKANO YOUSUKE!");
+	i = mlx_string_put(mlx, mlx_win, 10, 10, 0xfff, "RAZA Sharuku Youseke!");
+	i = mlx_get_color_value(mlx, 0xFFFFF0);
 	mlx_loop(mlx);
 }
